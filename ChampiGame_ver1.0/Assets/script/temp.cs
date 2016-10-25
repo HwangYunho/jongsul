1ef478eb729674644672c558165e3089bcdfdf89
HwangYunho <hwang9yh@naver.com>
2016-10-25 14:16:19 +0900

    Revert "deleted"
    This reverts commit 10e17d3ae387fd9b3b9e51111107e012a8590059.

---
+++ ChampiGame_ver1.0/Assets/script/temp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class temp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.DeleteKey("Name");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ ChampiGame_ver1.0/Assets/script/torturing.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(BoxCollider))]	//or other collider
+public class torturing : MonoBehaviour {
+
+    private bool onOff = false;
+    private bool uncheck = true;
+    public GameObject champi;
+    public GameObject gamemode;
+    private int desu;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {   // Update is called once per frame
+        desu = gamemode.GetComponent<modeSelect>().gameMode;
+        if (onOff && desu == 1)
+        {
+            onOff = false;
+            gamemode.GetComponent<GameManager>().HealthMinus(1);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (!onOff&&desu==1)
+            onOff = true;
+    }
+}
