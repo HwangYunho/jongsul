b0025713fc7872ff7434bc27920b514864ab7b90
Shin Seong Cheol <JuneShin@1s-MacBook-Pro.local>
2016-11-27 22:22:45 +0900

    version5 prototype
    aa

---
+++ version5/Assets/script/ujichaCollide.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//ssc
+public class ujichaCollide : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Jissou")){
+			this.gameObject.SetActive(false);
+		}
+	}
+}
