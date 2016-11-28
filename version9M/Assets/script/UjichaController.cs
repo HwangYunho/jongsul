13c74af0348aa35a75945b7844e6db51f90be5ff
HwangYunho <hwang9yh@naver.com>
2016-11-28 23:39:10 +0900

    modify

---
+++ version9M/Assets/script/UjichaController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UjichaController : MonoBehaviour {
+
+    public Animator anim;
+    public float controllTimer = 2f;
+    public int a;
+    public bool stop = true;
+
+    public float speed = 2f;
+
+    public GameObject movepoint;
+	// Use this for initialization
+	void Start () {
+        controllTimer = 3f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        controllTimer -= Time.deltaTime;
+
+        anim.SetBool("w", !stop);
+
+        this.transform.LookAt(movepoint.transform);
+        
+        if (controllTimer < 0 && stop)
+        {
+            controllTimer = 3f;
+            a = Random.Range(0, 2);
+            if (a == 0)
+            {
+                putmovepoint();
+            }
+        }
+        else if(!stop)
+        {
+            this.transform.position += (movepoint.transform.position - this.transform.position).normalized * speed * Time.deltaTime;
+            if(Vector3.Distance(this.transform.position, movepoint.transform.position) < 0.1f)
+            {
+                stop = true;
+                controllTimer = 3f;
+            }
+        }
+    }
+    void putmovepoint()
+    {
+        stop = false;
+        movepoint.transform.position = this.transform.position + new Vector3(Random.RandomRange(-2f,2f),0, Random.RandomRange(-2f, 2f));
+
+        if (movepoint.transform.position.z < -2f)
+        {
+            movepoint.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -2f);
+        }
+        if (movepoint.transform.position.z > 2f)
+        {
+            movepoint.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 2f);
+        }
+        if (movepoint.transform.position.y < 0)
+        {
+            movepoint.transform.position = new Vector3(this.transform.position.x, 1f, this.transform.position.z);
+        }
+        if (movepoint.transform.position.x < -4f)
+        {
+            movepoint.transform.position = new Vector3(-4f, this.transform.position.y, this.transform.position.z);
+        }
+        if (movepoint.transform.position.x > 4f)
+        {
+            movepoint.transform.position = new Vector3(4f, this.transform.position.y, this.transform.position.z);
+        }
+        if (movepoint.transform.position.y > 6f)
+        {
+            movepoint.transform.position = new Vector3(this.transform.position.x, 5f, this.transform.position.z);
+        }
+    }
+}
---
+++ version9M/Assets/script/ujichaCollide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ujichaCollide : MonoBehaviour {
+
+	public GameObject cha1;
+	//public bool mouseDown = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+		{
+			DestroyObject(cha1);
+			GameManager.instance.point += 100;
+		}
+
+	}
+}
