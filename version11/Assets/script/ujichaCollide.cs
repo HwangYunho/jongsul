f11f6c7f8bc211a1384c8b90bb8e2f73181bb1f2
HwangYunho <hwang9yh@naver.com>
2016-11-29 17:19:00 +0900

    version11

---
+++ version11/Assets/script/ujichaCollide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//ssc
+public class ujichaCollide : MonoBehaviour {
+	
+	void Update()
+	{
+		
+	}
+
+	void OnMouseEnter()
+	{
+        //this.gameObject.SetActive (false);
+        //Invoke("destory",0.5f);
+        destory();
+        GameManager.instance.point += 100;
+		GameManager.instance.numOfUjicha--;
+	}
+
+	void destory(){
+		Destroy(this.gameObject);
+	}
+}
+
+
